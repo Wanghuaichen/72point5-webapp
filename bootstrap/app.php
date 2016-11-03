67079717979b7e9941517f90a5dda3a2d9aa4aab
addisonbgross <addisonbgross@gmail.com>
2016-11-02 19:39:44 -0700

    Resetting public dir changing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,14 +18,9 @@ try {
 | application as an "IoC" container and router for this framework.
 |
 */
-if ($_ENV['APP_ENV'] == "local") {
-	$realpath = __DIR__.'/../';
-} else {
-	$realpath = __DIR__.'/../public/';
-}
 
 $app = new Laravel\Lumen\Application(
-	realpath($realpath)
+	realpath(__DIR__.'/../')
 );
 
 // allow for DB quickness
