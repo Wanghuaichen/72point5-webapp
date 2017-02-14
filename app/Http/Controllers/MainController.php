222e6a1c5162447403e3686f20a18b408c42a3a1
addisonbgross <addisonbgross@gmail.com>
2017-02-14 13:37:45 -0800

    Added end points for accel and normal samples

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -14,31 +14,48 @@ class MainController extends Controller
     public function getNormalSamples() 
     {
 		$maxSamples = 10;
-        return app('db')->select("SELECT * FROM normal_sample ORDER BY timestamp ASC LIMIT {$maxSamples}");
+        return app('db')->select("SELECT * FROM normal_sample ORDER BY timestamp DESC LIMIT {$maxSamples}");
     }
 
     public function getAccelSamples() 
     {
 		$maxSamples = 10;
-        return app('db')->select("SELECT * FROM accel_sample ORDER BY timestamp ASC LIMIT {$maxSamples}");
+        return app('db')->select("SELECT * FROM accel_sample ORDER BY timestamp DESC LIMIT {$maxSamples}");
     }
 
 	public function newRawSample(Request $request)
 	{
+		$NORMAL_SAMPLE_TYPE = 3;
+		$ACCEL_SAMPLE_TYPE = 4;
+
 		$data = $request->input();
 		foreach ($data as $key => $value) {
 			/*** IN HERE WE CAN DO VALIDATION OF INPUTS ***/
 		}
 
-		app('db')->table('normal_sample')->insert([
-			[
-				'timestamp'  => $data['timestamp'],
-				'body_temp'  => $data['body_temp'],
-				'ext_temp'   => $data['ext_temp'],
-				'heart_rate' => $data['heart_rate'],
-				'error'		 => $data['error'],
-				'cow_id'	 => $data['cow_id']
-			]	
-		]);
+		if ($data['packetType'] == $NORMAL_SAMPLE_TYPE) {
+			app('db')->table('normal_sample')->insert([
+				[
+					'timestamp'  => $data['timestamp'],
+					'body_temp'  => (($data['objtemp_h'] & 0xff) << 8) | ($data['objtemp_l'] & 0xff),
+					'ext_temp'   => (($data['ambtemp_h'] & 0xff) << 8) | ($data['ambtemp_l'] & 0xff),
+					'heart_rate' => (($data['hrate_high'] & 0xff) << 8) | ($data['hrate_low'] & 0xff),
+					'error'		 => $data['errcode'],
+					'cow_id'	 => $data['cowID']
+				]	
+			]);
+
+		} else if ($data['packetType'] == $ACCEL_SAMPLE_TYPE) {
+			app('db')->table('normal_sample')->insert([
+				[
+					'timestamp'  => $data['timestamp'],
+					'x'			 => $data['xaxis'],
+					'y'			 => $data['yaxis'],
+					'z'			 => $data['zaxis'],
+					'error'		 => $data['errcode'],
+					'cow_id'	 => $data['cowID']
+				]	
+			]);
+		}
 	}
 }
